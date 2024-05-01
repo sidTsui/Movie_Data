580dd8cdca9607c0e9a973327b5ce0994b93a1fc
Maria Katris <77807527+mariakatris@users.noreply.github.com>
2024-05-01 14:52:29 -0400

    Update sameDirector.php

--- 455 final proj/sameDirector.php
+++ 455 final proj/sameDirector.php
@@ -1,10 +1,12 @@
 <?php
 	try {
 		require('../pdo_connect.php'); //Connect to the database
-		$sql = "SELECT DISTINCT m1.Title AS MovieTitle1, m2.Title AS MovieTitle2, d.FN, d.LN
-		FROM MOVIE AS m1
-		JOIN MOVIE AS m2 ON m1.DirectorID = m2.DirectorID AND m1.Title != m2.Title AND m1.Title < m2.Title
-		JOIN MAIN_DIRECTOR AS d ON m1.DirectorID = d.DirectorID;";
+		$sql = "SELECT d.FN, d.LN, GROUP_CONCAT(m1.Title ORDER BY m1.Title) AS DirectedMovies
+FROM MAIN_DIRECTOR AS d
+JOIN MOVIE AS m1 ON d.DirectorID = m1.DirectorID
+JOIN MOVIE AS m2 ON d.DirectorID = m2.DirectorID AND m1.MovieID != m2.MovieID
+GROUP BY d.DirectorID
+HAVING COUNT(*) > 1;";
 		$result = $dbc->query($sql);	
 	} catch (PDOException $e){
 			echo $e->getMessage();
@@ -21,17 +23,15 @@
 
 	<table>
 		<tr>
-			<th>Movie 1</th>
-			<th>Movie 2</th>
 			<th>Director First Name</th>
 			<th>Director Last Name</th>
+			<th>Movies</th>
 		</tr>
 		<?php foreach ($result as $row) { ?>
 			<tr>
-				<td><?php echo $row['MovieTitle1']; ?></td>
-				<td><?php echo $row['MovieTitle2']; ?></td>
 				<td><?php echo $row['DirectorFirstName']; ?></td>
 				<td><?php echo $row['DirectorLastName']; ?></td>
+				<td><?php echo $row['DirectedMovies']; ?></td>
 			</tr>
 		<?php } ?>
 	</table>
