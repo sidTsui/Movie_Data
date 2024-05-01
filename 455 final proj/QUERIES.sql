f2861c87a737afe71daeeb38f2356830e9e3b354
Maria Katris <77807527+mariakatris@users.noreply.github.com>
2024-05-01 16:26:09 -0400

    Update QUERIES.sql

--- 455 final proj/QUERIES.sql
+++ 455 final proj/QUERIES.sql
@@ -19,28 +19,16 @@ LEFT JOIN PLATFORM AS p ON io.PlatformID = p.PlatformID
 WHERE m.ReleaseYear = 2001
 AND m.CountryCreated = 'USA';
 
-********************
 /*Requirement: Self-join: 
 
 Description: Find all movies where director of movie 1 matches the director of movie 2.
-Implementation: joins MOVIES w/ MOVIES based on DirectorId
- needs php */
-SELECT DISTINCT m1.Title AS MovieTitle1, m2.Title AS MovieTitle2, d.FN, d.LN
-FROM MOVIE AS m1
-JOIN MOVIE AS m2 ON m1.DirectorID = m2.DirectorID AND m1.Title != m2.Title AND m1.Title < m2.Title
-JOIN MAIN_DIRECTOR AS d ON m1.DirectorID = d.DirectorID;
-
-**********************
-
-/* i think this is better self join than one above*/
-
-SELECT d.FN, d.LN, GROUP_CONCAT(m1.Title ORDER BY m1.Title) AS DirectedMovies
+Implementation: joins MOVIES w/ MOVIES based on DirectorId*/
+SELECT d.FN, d.LN, GROUP_CONCAT( DISTINCT m1.Title ORDER BY m1.Title) AS DirectedMovies
 FROM MAIN_DIRECTOR AS d
 JOIN MOVIE AS m1 ON d.DirectorID = m1.DirectorID
 JOIN MOVIE AS m2 ON d.DirectorID = m2.DirectorID AND m1.MovieID != m2.MovieID
 GROUP BY d.DirectorID
 HAVING COUNT(*) > 1;
-**********************
 
 
 /*Requirement: Aggregate function: multiple rows grouped into one 
